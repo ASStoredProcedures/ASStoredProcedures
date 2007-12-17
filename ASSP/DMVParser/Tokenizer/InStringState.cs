e13f7fd796822819ba250badc44eab6715f55d32
dgosbell_cp <SND\dgosbell_cp>
2007-12-17 12:48:13 +0000

    Adding code headers to some of the DMV helper classes and fixing column typing in the Discover and DMV functions.

--- ASSP/DMVParser/Tokenizer/InStringState.cs
+++ ASSP/DMVParser/Tokenizer/InStringState.cs
@@ -1,3 +1,20 @@
+/*============================================================================
+  File:    DefaultState.cs
+
+  Summary: The class in this file is part of a state machine that tokenizes
+           the DMV select syntax. It is responsible for handling strings.
+
+  Date:    December 14, 2007
+
+  ----------------------------------------------------------------------------
+  This file is part of the Analysis Services Stored Procedure Project.
+  http://www.codeplex.com/Wiki/View.aspx?ProjectName=ASStoredProcedures
+  
+  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+  PARTICULAR PURPOSE.
+============================================================================*/
 using System;
 using System.Collections.Generic;
 using System.Text;
