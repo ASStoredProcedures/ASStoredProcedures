41ec5b148904ead407f92e41edab4ada4406925d
cwebb_cp <SND\cwebb_cp>
2008-01-28 21:39:55 +0000

    Adding first draft of WriteToProfiler

---
+++ ASSP/WriteToProfiler.cs
@@ -0,0 +1,46 @@
+/*============================================================================
+  File:    WriteToProfiler.cs
+
+  Summary: Provides the ability to write custom comments to Profiler from MDX
+
+  Date:    January 28th, 2008
+
+  ----------------------------------------------------------------------------
+  This file is part of the Analysis Services Stored Procedure Project.
+  http://www.codeplex.com/ASStoredProcedures
+  
+  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+  PARTICULAR PURPOSE.
+============================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AnalysisServices.AdomdServer;
+
+namespace ASStoredProcs
+{
+    public class WriteToProfiler
+    {
+        public static MDXValue WriteComment(int eventSubClass, int numberData, string textData)
+        {
+            Context.TraceEvent(eventSubClass, numberData, textData);
+            return (MDXValue) null;
+        }
+
+
+        public static MDXValue WriteComment(MDXValue inputValue, int eventSubClass, int numberData, string textData)
+        {
+            Context.TraceEvent(eventSubClass, numberData, textData);
+            return inputValue;
+        }
+
+        public static MDXValue WriteComment(Set inputSet, int eventSubClass, int numberData, string textData)
+        {
+            Context.TraceEvent(eventSubClass, numberData, textData);
+            return inputSet;
+        }
+    }
+}
