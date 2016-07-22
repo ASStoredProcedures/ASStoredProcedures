53db7ecf5fc808e6a9a57e24de7b5db0366dec6b
dgosbell_cp <dgosbell@users.noreply.github.com>
2016-07-22 23:07:33 +0000

    fixing 2016 references and changing build output to uppercase

--- ASSP/BuildScript/build.cmd
+++ ASSP/BuildScript/build.cmd
@@ -3,8 +3,8 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2008"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2012"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2014"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2016"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild2008.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2008"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild2012.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2012"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild2014.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2014"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild2016.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2016"
 pause
