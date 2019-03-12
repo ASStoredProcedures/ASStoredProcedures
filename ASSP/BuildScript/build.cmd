22cb8a120e1a8dc2814d448ba9db7123480b6e65
Darren Gosbell <dgosbell@users.noreply.github.com>
2019-03-13 09:51:32 +1100

    Adding 2017 project and dlls and updating gitignore

--- ASSP/BuildScript/build.cmd
+++ ASSP/BuildScript/build.cmd
@@ -7,4 +7,5 @@ if "%config%" == "" (
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /p:Platform="x64" /m /v:M /fl /flp:LogFile=msbuild2012.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2012"
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /p:Platform="x64" /m /v:M /fl /flp:LogFile=msbuild2014.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2014"
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /p:Platform="x64" /m /v:M /fl /flp:LogFile=msbuild2016.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2016"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" build.msproj /p:Configuration="%config%" /p:Platform="x64" /m /v:M /fl /flp:LogFile=msbuild2017.log;Verbosity=Normal /nr:false /target:ZipFile /p:Edition="2017"
 pause
